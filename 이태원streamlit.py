3213aa511e38c4ef37918c9d5bfe2074dadb2ec9
hiy0406 <120291503+hiy0406@users.noreply.github.com>
2022-12-15 02:04:31 +0900

    Update 이태원streamlit.py

--- 이태원streamlit.py
+++ 이태원streamlit.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import altair as alt
 
-font_path = "./Nanum.ttf"
+font_path = "./NanumCoding.ttf"
 font_family=fm.FontProperties(fname=font_path).get_name()
 plt.rcParams["font.family"] = font_family
 
